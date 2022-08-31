6a950e51a8d87a078c212184e1286d66f6e5822d
Shashank rawat <63609785+shashank-afk@users.noreply.github.com>
2022-08-31 22:10:10 +0530

    Added themes folder

---
+++ lib/theme/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static Color get bg => const Color(0xFFF9F9F9);
+
+  static Color get bg2 => const Color(0xFFF7FFF8);
+
+  static Color get card => const Color(0xFFFFFFFF);
+
+  static Color get t1 => const Color(0xFF000000);
+
+  static Color get t2 => const Color(0xFF818181);
+
+  static Color get t3 => const Color(0xFFFFFFFF);
+
+  static Color get accent => const Color(0xFF72CC50);
+
+  static Color get red => const Color(0xFFFF6969);
+
+  static Color get lightGreen => const Color(0xFFDDFFD0);
+
+  static Color get darkGreen => const Color(0xFF307E13);
+
+  static Color get onAccent => const Color(0xFFFFFFFF);
+
+  static Color get cardShadow => const Color(0xFFB5FF98).withOpacity(0.3);
+}
